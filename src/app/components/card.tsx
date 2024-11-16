cf86b328c2927db02505e2323d9a24973d9d2fa0
Mariam-Aamir <ummeareesh691@gmail.com>
2024-11-16 19:39:55 +0500

    first commit

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,5 +1,5 @@
 
-const Card = (props: any) => {
+const Card = (props: { name: string; Id: number; day: string })  => {
   return (
     <div className="flex flex-col pl-5 items-start justify-center border-2 w-52 h-24 -mt-[20px]">
       <h2>Name: {props.name}</h2>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 
 import Card from "./components/card";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
 
   return (
     <main className="flex flex-col bg-cyan-500 text-black" >
-      <div className="flex flex-col gap-5 items-center justify-between py-6 ">
-      
-      </div>
-      <div className="max-w-7xl mt-16 flex flex-col items-center justify-center mx-auto -mt-[20px]">
+      <div className="flex flex-col gap-5 items-center justify-between py-6 "></div>
+      <div className="max-w-7xl  flex flex-col items-center justify-center mx-auto -mt-[20px]">
         <h1 className="font-[poppins] text-[3.34rem] font-extrabold text-red-900 dark:text-white md:text-5xl lg:text-6xl">
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-pink-600 from-yellow-400">
             Props Data
@@ -21,10 +15,14 @@ export default function Home() {
           <br/>
           <br/>
         </h1>
-        <Card name={"Mariam"} Id={188224} day={"Monday 2pm - 5pm"} /><br/>
-        <Card name={"Nimra"} Id={166548} day={"Monday 2pm - 5pm"} /><br/>
-        <Card name={"Yusra"} Id={166548} day={"Monday 2pm - 5pm"} /><br/>
-        <Card name={"Huzaifa"} Id={166548} day={"Monday 2pm - 5pm"} /><br/>
+        <Card name={"Mariam"} Id={188224} day={"Monday 2pm - 5pm"} />
+        <br/>
+        <Card name={"Nimra"} Id={166548} day={"Monday 2pm - 5pm"} />
+        <br/>
+        <Card name={"Yusra"} Id={166548} day={"Monday 2pm - 5pm"} />
+        <br/>
+        <Card name={"Huzaifa"} Id={166548} day={"Monday 2pm - 5pm"} />
+        <br/>
       </div>
     </main>
   );
